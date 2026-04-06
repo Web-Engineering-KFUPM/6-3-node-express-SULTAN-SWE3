5a21474447deb35a67e21bbb6e06d2dfd1449e2c
SULTAN-SWE <sultans8477@gmail.com>
2026-04-06 08:47:45 +0300

    TODO 6 is done

--- 6-3-node-express/backend/server.js
+++ 6-3-node-express/backend/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import { getRandomQuote } from "./quotes.js";
 
 const app = express();
 
@@ -18,9 +19,16 @@ app.use(morgan("dev"));
 
 
 // TODO 6.1: Create root route "/"
+app.get("/", (req, res) => {
+  res.send("Welcome to the Random Quote API");
+});
 
 
 // TODO 6.2: Create "/api/quote" route
+app.get("/api/quote", (req, res) => {
+  const quote = getRandomQuote();
+  res.json({ quote });
+});
 
 
 // TODO 7: Start server using app.listen
